b36ebf53e3e55fdf0c812113f6b483a40708e9f0
Satya <dt.satyam@gmail.com>
2022-11-24 17:54:25 +0530

    Added a new function called sum

--- data_processing.py
+++ data_processing.py
@@ -1,3 +1,9 @@
 print("Adding all data processing code related to fraud detection")
 
-print("Commit check")
+a = 5
+b = 7
+def sum():
+    c = a+b
+    return a
+
+print("Added anew function Sum which is not correct")
